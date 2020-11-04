b63af5913f4cb5c24480e55bebc38c81ea8ac1f5
raymond gitonga <raytosh95@gmail.com>
2020-11-04 20:24:55 +0300

    Day #4 solution

---
+++ 7kyu/FixStringCae.go
@@ -0,0 +1,44 @@
+package _kyu
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Source : https://www.codewars.com/kata/5b180e9fedaa564a7000009a
+
+/**********************************************************************************
+ *
+ * In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your
+ * task is to convert that string to either lowercase only or uppercase only based on:
+ * make as few changes as possible.
+ * if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+ * For example:
+ * solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+ * solve("CODe") = "CODE". Uppercase characters > lowercase. Change only the "e" to uppercase.
+ * solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+ **********************************************************************************/
+
+func solve(str string) string {
+	var upper int
+	var lower int
+
+	strArr := strings.Split(str, "")
+
+	for _, st := range strArr {
+		if strings.ToLower(st) == st {
+			lower += 1
+		} else {
+			upper += 1
+		}
+	}
+
+	fmt.Println(upper, lower)
+
+	if lower == upper || lower > upper {
+		return strings.ToLower(str)
+	} else {
+		return strings.ToUpper(str)
+	}
+
+}
